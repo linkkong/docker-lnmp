ddd4dee0b50c7235c3c9d6ddc71efb7d4f856ac5
Mengxiangcun <mengxiangcun@100tal.com>
2018-07-10 16:15:56 +0800

    增加nginx日志输出，数据库主从，php7.2,phpmyadmin

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -5,5 +5,5 @@
 # rm -rf /etc/default/locale
 # env >> /etc/default/locale
 /etc/init.d/cron start
-php-fpm
+php-fpm -c /usr/local/etc/php-fpm.ini
 # exec "$@"
--- php/php7.2/entrypoint.sh
+++ php/php7.2/entrypoint.sh
@@ -5,6 +5,6 @@
 # rm -rf /etc/default/locale
 # env >> /etc/default/locale
 /etc/init.d/cron start
-service supervisor start
+service supervisor restart
 php-fpm -c /usr/local/etc/php-fpm.ini
 # exec "$@"
